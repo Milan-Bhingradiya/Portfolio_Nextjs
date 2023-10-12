956437441bd907430c644c4af3124c7723ff6a42
Milan op <milanbhingradiya00@gmail.com>
2023-10-13 00:45:22 +0530

    init commit

---
+++ app/milan/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function milan() {
+  return (
+    <div className='flex justify-center items-center h-screen' ><h1 className=' text-6xl '>Milan OP </h1></div>
+  )
+}
