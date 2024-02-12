a3fb4dfc063ba411b39d76fa9d7f28f6dd04e348
Milan op <milanbhingradiya00@gmail.com>
2024-02-12 15:31:34 +0530

    .end added

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import axios from 'axios';
 
 const myaxios = axios.create({
-  baseURL: "http://localhost:4000/api/v1" // Corrected baseURL
+  baseURL: ` ${process.env.BASE_URL}|| http://localhost:4000/api/v1` // Corrected baseURL
 });
 
 export default myaxios;
