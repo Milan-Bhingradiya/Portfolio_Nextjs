02305d01703f5ee66127b5c303a0d64ab8d4eb8a
Milan Bhingradiya <milanbhingradiya00@gmail.com>
2025-02-22 17:25:36 +0530

    solve error in education page

--- src/app/Education/page.tsx
+++ src/app/Education/page.tsx
@@ -76,7 +76,7 @@ export default function Education() {
   return (
     <div
       className={`${
-        isWide ? "w-[90vw]" : "w-[100vw]"
+        isWide ? "w-[100vw]" : "w-[100vw]"
       } relative bg-[#0a192f] min-h-screen text-white`}
     >
       <Navbar />
@@ -138,6 +138,8 @@ export default function Education() {
 
       {/* Background Animation */}
       <motion.div
+
+
         animate={{
           background: [
             "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.21) 15.74%, rgba(232, 121, 249, 0.16) 56.49%, rgba(79, 70, 229, 0.2) 115.91%)",
@@ -149,7 +151,7 @@ export default function Education() {
           repeat: Number.POSITIVE_INFINITY,
           repeatType: "reverse",
         }}
-        className="absolute inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[600px]"
+        className="absolute mt-[100px] inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[600px]"
       />
     </div>
   );
